a9aed7404a1cd1ca6f410c317a0652eea8fa6894
Juan Jose Garcia Ripoll <jjgarcia@JJGR.(none)>
2008-09-06 09:54:54 +0200

    Document random number generator

--- include/tensor/rand.h
+++ include/tensor/rand.h
@@ -8,8 +8,18 @@
 
 #include <tensor/numbers.h>
 
+/** Reset the random number generator. If the environment variable
+    RANDSEED is defined, that value is used. Otherwise we try to
+    gather enough random numbers, either from /dev/urandom, from
+    the clock or from other sources. */
 void rand_reseed();
 
+/** Returns a random number. If the type is an integral one, the range
+    is the whole integer range; if the type is a complex or floating point
+    type, then the range is a n-dimensional cube with a corner on [0,...,0]
+    and extending along the positive axis without reaching value of 1 on
+    only direction.
+*/
 template<class number> number rand() {
   return static_cast<number>(rand<double>());
 }
