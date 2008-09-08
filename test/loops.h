26bd3c196d86bf020e1e241ee35921db92da8a5c
Juan Jose Garcia Ripoll <jjgarcia@jjgr-2.local>
2008-09-08 15:04:20 +0200

    Reorder tests for readability

--- test/loops.h
+++ test/loops.h
@@ -27,8 +27,8 @@ void unchanged(const Tensor &t1, const Tensor &t2, size_t expected_refs = 2) {
     if ((t1.size() + t2.size()) == 0)
 	return;
     EXPECT_EQ(t1.begin_const(), t2.begin_const());
-    EXPECT_EQ(t1.ref_count(), expected_refs);
-    EXPECT_EQ(t2.ref_count(), expected_refs);
+    EXPECT_EQ(expected_refs, t1.ref_count());
+    EXPECT_EQ(expected_refs, t2.ref_count());
 }
 
 /*
