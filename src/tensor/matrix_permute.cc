723498019be1ce058c9b1dd9a24d78846a93fb11
Juan Jose Garcia Ripoll <jjgarcia@juanjo-imac.local>
2009-01-23 18:03:34 +0100

    Do not invoke the permutation operators for empty tensors.

--- src/tensor/matrix_permute.cc
+++ src/tensor/matrix_permute.cc
@@ -22,6 +22,8 @@ namespace tensor {
       for (index j = a2; j--; jk_b++) {
         typename Tensor<n>::iterator ijk_b = jk_b;
         for (index i = a1; i--; ijk_a++, ijk_b += a2) {
+          //assert(ijk_a >= a.begin() && ijk_a < a.end());
+          //assert(ijk_b >= b.begin() && ijk_b < b.end());
           *ijk_b = *ijk_a;
         }
       }
@@ -31,21 +33,25 @@ namespace tensor {
   template<typename n>
   void permute_23(Tensor<n> &b, const Tensor<n> &a, index a1, index a2, index a3, index a4)
   {
-    // In an abstract sense b(i,k,j,l) = a(i,j,k,l)
+    // In an abstract sense
+    // b(i,k,j,l) = a(i,j,k,l)
+    // b(a1,a3,a2,a4) = a(a1,a2,a3,a4)
     // Both tensors are stored in row-major order. So for A we have
     // (0,0,0), (1,0,0), ... (2,1,0), ... (a1-1,a2-1,a3-1)
     //
     typename Tensor<n>::const_iterator ijkl_a = a.begin();
     typename Tensor<n>::iterator l_b = b.begin();
-    index a12 = a1*a2;
-    index a123 = a12*a3;
-    for (; a4--; l_b += a123) {
+    index a13 = a1*a3;
+    index a132 = a13*a2;
+    for (; a4--; l_b += a132) {
       typename Tensor<n>::iterator kl_b = l_b;
       for (index k = a3; k--; kl_b += a1) {
         typename Tensor<n>::iterator kjl_b = kl_b;
-        for (index j = a2; j--; kjl_b += a12) {
+        for (index j = a2; j--; kjl_b += a13) {
           typename Tensor<n>::iterator ikjl_b = kjl_b;
           for (index i = a1; i--; ijkl_a++, ikjl_b++) {
+            //assert(ijkl_a >= a.begin() && ijkl_a < a.end());
+            //assert(ikjl_b >= b.begin() && ikjl_b < b.end());
             *ikjl_b = *ijkl_a;
           }
         }
@@ -57,24 +63,28 @@ namespace tensor {
   void permute_24(Tensor<n> &b, const Tensor<n> &a, index a1, index a2, index a3,
                   index a4, index a5)
   {
-    // In an abstract sense b(i,l,k,j,m) = a(i,j,k,l,m)
+    // In an abstract sense
+    // b(i,l,k,j,m) = a(i,j,k,l,m)
+    // b(a1,a4,a3,a2,a5) = a(a1,a2,a3,a4,a5)
     // Both tensors are stored in row-major order. So for A we have
     // (0,0,0), (1,0,0), ... (2,1,0), ... (a1-1,a2-1,a3-1)
     //
     typename Tensor<n>::const_iterator ijklm_a = a.begin();
     typename Tensor<n>::iterator m_b = b.begin();
-    index a12 = a1*a2;
-    index a123 = a12*a3;
-    index a1234 = a123*a4;
-    for (; a5--; m_b += a1234) {
+    index a14 = a1*a4;
+    index a143 = a14*a3;
+    index a1432 = a143*a2;
+    for (; a5--; m_b += a1432) {
       typename Tensor<n>::iterator lm_b = m_b;
       for (index l = a4; l--; lm_b += a1) {
         typename Tensor<n>::iterator lkm_b = lm_b;
-        for (index k = a3; k--; lkm_b += a12) {
+        for (index k = a3; k--; lkm_b += a14) {
           typename Tensor<n>::iterator lkjm_b = lkm_b;
-          for (index j = a2; j--; lkjm_b += a123) {
+          for (index j = a2; j--; lkjm_b += a143) {
             typename Tensor<n>::iterator ilkjm_b = lkjm_b;
             for (index i = a1; i--; ijklm_a++, ilkjm_b++) {
+              //assert(ilkjm_b < b.begin() || b.end() <= ilkjm_b);
+              //assert(ijklm_a < a.begin() || a.end() <= ijklm_a);
               *ilkjm_b = *ijklm_a;
             }
           }
@@ -107,12 +117,14 @@ namespace tensor {
     std::swap(new_dims.at(n1), new_dims.at(n2));
     Tensor<n> output(new_dims);
 
-    if (a3 > 1) {
-      permute_24(output, a, a1,a2,a3,a4,a5);
-    } else if (a1 > 1) {
-      permute_23(output, a, a1,a2,a4,a5);
-    } else {
-      permute_12(output, a, a2,a4,a5);
+    if (output.size()) {
+      if (a3 > 1) {
+        permute_24(output, a, a1,a2,a3,a4,a5);
+      } else if (a1 > 1) {
+        permute_23(output, a, a1,a2,a4,a5);
+      } else {
+        permute_12(output, a, a2,a4,a5);
+      }
     }
     return output;
   }
