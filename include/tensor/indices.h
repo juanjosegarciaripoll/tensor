38643553796ce34c8a9729c84820680c68725c7f
Juan Jose Garcia Ripoll <jjgarcia@jjgr-2.local>
2009-01-26 10:18:00 +0100

    Added ranges

--- include/tensor/indices.h
+++ include/tensor/indices.h
@@ -11,33 +11,100 @@
 
 namespace tensor {
 
-class ListGenerator  {};
+  class ListGenerator  {};
 
-extern ListGenerator gen;
+  extern ListGenerator gen;
 
-template<typename elt_t>
-std::list<elt_t> &operator<<(std::list<elt_t> &l, const elt_t &x) {
-  l.push_back(x);
-  return l;
-}
+  template<typename elt_t>
+  std::list<elt_t> &operator<<(std::list<elt_t> &l, const elt_t &x) {
+    l.push_back(x);
+    return l;
+  }
 
-template<typename elt_t>
-std::list<elt_t> operator<<(const ListGenerator &g, const elt_t &x) {
-  std::list<elt_t> output;
-  output.push_back(x);
-  return output;
-}
+  template<typename elt_t>
+  std::list<elt_t> operator<<(const ListGenerator &g, const elt_t &x) {
+    std::list<elt_t> output;
+    output.push_back(x);
+    return output;
+  }
 
-class Indices : public Vector<index> {
-public:
-  Indices() : Vector<index>() {}
+  class Indices : public Vector<index> {
+  public:
+    Indices() : Vector<index>() {}
 
-  explicit Indices(index size) : Vector<index>(size) {}
+    explicit Indices(index size) : Vector<index>(size) {}
 
-  bool operator==(const Indices &other) const;
-};
+    bool operator==(const Indices &other) const;
+  };
 
-extern template class Vector<index>;
+  extern template class Vector<index>;
+
+  //////////////////////////////////////////////////////////////////////
+  // RANGE OF INTEGERS
+  //
+
+  class Range {
+  public:
+    typedef enum {
+      Full,
+      Stepped,
+      List,
+      None
+    } Type;
+
+    Range() : type(Full), i0(0), i1(-1), di(1) {}
+
+    Range(const Indices &av): type(List), v(av) {}
+
+    Range(index ndx) : type(Stepped), i0(ndx), i1(ndx), di(1) {}
+
+    Range(index start, index end) : type(Stepped), i0(start), i1(end), di(1) {}
+
+    Range(index start, index end, index delta) :
+	type(Stepped), i0(start), i1(end), di(delta)
+    {}
+
+    index size(index dimension) const;
+
+  private:
+    Type type;
+    index i0, i1, di;
+    Indices v;
+
+    friend class RangeProduct;
+    index *to_offsets(index increment, index dimension) const;
+  };
+
+  //////////////////////////////////////////////////////////////////////
+  // TENSOR PRODUCT OF RANGES
+  //
+
+  class RangeProduct {
+  public:
+    RangeProduct();
+    ~RangeProduct();
+
+    RangeProduct &operator<<(const Range &r);
+
+    index size() const { return size_; }
+    bool begin(const Indices &limits) const;
+    bool next() const;
+    index offset() const;
+    bool is_empty() const { return !size(); }
+    Indices &dimensions() const;
+
+  private:
+    std::list<Range> ranges_;
+    index size_;
+    index rank_;
+    index **offsets_;
+    index *counters_;
+    index *limits_;
+
+    RangeProduct(const RangeProduct &t); /* Deactivated */
+    void reshape(const Indices &dims, Range **r);
+  };
+
 
 }; // namespace
 
---
+++ src/views/range.cc
@@ -0,0 +1,86 @@
+// -*- mode: c++; fill-column: 80; c-basic-offset: 2; indent-tabs-mode: nil -*-
+//
+// Copyright 2008, Juan Jose Garcia-Ripoll
+//
+
+#include <iostream>
+#include <tensor/indices.h>
+
+namespace tensor {
+
+  index
+  Range::size(index dim) const
+  {
+    if (dim == 0)
+      return 0;
+    switch (type) {
+    case Full: {
+      return dim;
+    }
+    case Stepped: {
+      index k0 = i0 < 0? dim + i0 : i0;
+      index k1 = i1 < 0? dim + i1 : i1;
+      if (k1 >= dim || k0 >= dim) {
+        std::cerr << "In Range(): The interval [" << k0 << "," << k1
+                  << "] exceeds the limits of the\nselected index, which go from 0 to " << (dim-1);
+        abort();
+      }
+      return ((k1 - k0)/di + 1);
+    }
+    case List: {
+      return v.size();
+    }
+    default:
+      std::cerr << "Not a valid Range type " << type << '\n';
+      abort();
+      return 0;
+    }
+  }
+
+  index *
+  Range::to_offsets(index increment, index dimension) const
+  {
+    index k0, k1, dk;
+    index l = size(dimension);
+    index *p = new index[l];
+    switch (type) {
+    case Full:
+      for (k0 = 0, k1 = 0; k0 < dimension; k0++) {
+        p[k0] = k1;
+        k1 += increment;
+      }
+      break;
+    case Stepped:
+      k0 = i0 < 0? dimension + i0 : i0;
+      k1 = i1 < 0? dimension + i1 : i1;
+      if (k1 >= dimension || k0 >= dimension) {
+        std::cerr << "In Range(): The interval [" << k0 << "," << k1
+                  << "] exceeds the limits of the\nselected index, which go from 0 to "
+                  << (dimension-1) << std::endl;
+        abort();
+      }
+      dk = di;
+      for (index j = 0; k0 <= k1; k0+=dk, j++) {
+        p[j] = k0 * increment;
+      }
+      break;
+    case List:
+      for (index j = 0; j < l; j++) {
+        index ndx = v[j];
+        if (ndx >= dimension) {
+          std::cerr << "In Range(V), some of the elements of the index vector V exceed the\n"
+                    << "limits of the selected index, which go from 0 to " << dimension
+                    << std::endl;
+          abort();
+        }
+        p[j] = ndx * increment;
+      }
+      break;
+    default:
+      std::cerr << "Not a valid Range type " << type << '\n';
+      abort();
+    }
+    return p;
+  }
+
+} // namespace tensor
