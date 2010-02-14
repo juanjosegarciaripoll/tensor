887ba7ff895205c0b5af59e0f8015ee2632d585f
Juan Jose Garcia Ripoll <jjgarcia@jjgr-2.local>
2010-02-14 22:54:31 +0100

    Extend tests for ranges to two dimensions, with all types.

--- test/test_tensor_view.cc
+++ test/test_tensor_view.cc
@@ -12,6 +12,16 @@ namespace tensor_test {
 
   using namespace tensor;
 
+  IndexRange *range2(tensor::index i0, tensor::index i2, tensor::index i1)
+  {
+    tensor::index l = (i2 - i0) / i1 + 1;
+    Indices output(l);
+    for (int i = 0; i < l; i++, i0 += i1) {
+      output.at(i) = i0;
+    }
+    return new IndexRange(output);
+  }
+
   template<typename elt_t> void test_range1(Tensor<elt_t> &P) {
     Tensor<elt_t> Paux = P;
 
@@ -47,36 +57,73 @@ namespace tensor_test {
     unchanged(P, Paux);
   }
 
+  template<typename elt_t> Tensor<elt_t>
+  slow_range2(const Tensor<elt_t> &P,
+              tensor::index i0, tensor::index i2, tensor::index i1,
+              tensor::index j0, tensor::index j2, tensor::index j1)
+  {
+    Indices i(2);
+    i.at(0) = (i2 - i0) / i1 + 1;
+    i.at(1) = (j2 - j0) / j1 + 1;
+    Tensor<elt_t> t(i);
+    for (tensor::index i = i0, x = 0; i <= i2; i += i1, x++) {
+      for (tensor::index j = j0, y = 0; j <= j2; j += j1, y++) {
+        t.at(x,y) = P(i,j);
+      }
+    }
+    return t;
+  }
+
   template<typename elt_t> void test_range2(Tensor<elt_t> &P) {
+    tensor::index rows = P.dimension(0);
+    tensor::index cols = P.dimension(1);
     Tensor<elt_t> Paux = P;
 
-    for (tensor::index i = 0; i < P.dimension(0); i++) {
-      for (tensor::index j = 0; j < P.dimension(1); j++) {
+    for (tensor::index i = 0; i < rows; i++) {
+      for (tensor::index j = 0; j < cols; j++) {
         Tensor<elt_t> t = P(range(i), range(j));
         EXPECT_EQ(P.rank(), t.rank());
         EXPECT_EQ(1, t.size());
         EXPECT_EQ(t[0], P(i,j));
-        unchanged(P, Paux);
-
         Tensor<elt_t> t2 = P(range(i), range(j,j));
         EXPECT_EQ(t2, t);
-        unchanged(P, Paux);
-
         Tensor<elt_t> t3 = P(range(i,i), range(j,j));
         EXPECT_EQ(t3, t);
-        unchanged(P, Paux);
-
         Tensor<elt_t> t4 = P(range(i,i), range(j));
         EXPECT_EQ(t4, t);
-        unchanged(P, Paux);
-
         if (i+1 < P.dimension(0)) {
           Tensor<elt_t> t5 = P(range(i,i+1,2), range(j));
           EXPECT_EQ(t5, t);
-          unchanged(P, Paux);
+        }
+        Tensor<elt_t> t6 = P(range2(i,i+1,2), range(j));
+        EXPECT_EQ(t6, t);
+        Tensor<elt_t> t7 = P(range(i), range(j,j));
+        EXPECT_EQ(t7, t);
+      }
+    }
+    unchanged(P, Paux);
+
+    for (tensor::index i1 = 1; i1 < 4; i1++) {
+      for (tensor::index j1 = 1; j1 < 4; j1++) {
+        for (tensor::index i0 = 0; i0 < rows; i0++) {
+          for (tensor::index j0 = 0; j0 < cols; j0++) {
+            for (tensor::index i2 = i0; i2 < rows; i2++) {
+              for (tensor::index j2 = j0; j2 < cols; j2++) {
+                Tensor<elt_t> t1 = slow_range2(P, i0,i2,i1,j0,j2,j1);
+                Tensor<elt_t> t2 = P(range(i0,i2,i1), range(j0,j2,j1));
+                EXPECT_EQ(t2, t1);
+                Tensor<elt_t> t3 = P(range2(i0,i2,i1), range(j0,j2,j1));
+                EXPECT_EQ(t3, t1);
+                Tensor<elt_t> t4 = P(range(i0,i2,i1), range2(j0,j2,j1));
+                EXPECT_EQ(t4, t1);
+              }
+            }
+          }
         }
       }
     }
+    unchanged(P, Paux);
+
     Tensor<elt_t> t = P(range(), range());
     EXPECT_EQ(t, P);
     unchanged(P, Paux);
@@ -94,7 +141,8 @@ namespace tensor_test {
   //
 
   TEST(SliceTest, SliceRTensorTest) {
-    test_over_tensors<double>(test_range<double>);
+    //test_over_tensors<double>(test_range<double>,2,10,100);
+    test_over_all_tensors<double>(test_range<double>,2);
   }
 
   /////////////////////////////////////////////////////////////////////
@@ -102,7 +150,8 @@ namespace tensor_test {
   //
 
   TEST(SliceTest, SliceCTensorTest) {
-    test_over_tensors<cdouble>(test_range<cdouble>);
+    //test_over_tensors<cdouble>(test_range<cdouble>,2,10,100);
+    test_over_all_tensors<cdouble>(test_range<cdouble>,2);
   }
 
 } // namespace tensor_test
