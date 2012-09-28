e5d6a3586098cbc5be75ae05c34a5c99fb29a3c4
Juan Jose Garcia Ripoll <jjgarcia@users.sourceforge.net>
2012-09-28 20:50:44 +0200

    Added missing files for mps:tools

---
+++ src/mps/tools/build_E_matrix.hpp
@@ -0,0 +1,47 @@
+// -*- mode: c++; fill-column: 80; c-basic-offset: 2; indent-tabs-mode: nil -*-
+/*
+    Copyright (c) 2012 Juan Jose Garcia Ripoll
+
+    Tensor is free software; you can redistribute it and/or modify it
+    under the terms of the GNU Library General Public License as published
+    by the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Library General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*/
+
+#include <mps/tools.h>
+
+namespace mps {
+
+  using namespace tensor;
+
+  template<typename elt_t>
+  Tensor<elt_t>
+  build_E_matrix(const Tensor<elt_t> &A, tensor::index *a, tensor::index *b)
+  {
+    return build_E_matrix(A, A, a, b);
+  }
+
+  template<typename elt_t>
+  Tensor<elt_t>
+  build_E_matrix(const Tensor<elt_t> &A1, const Tensor<elt_t> &A2, tensor::index *l, tensor::index *r)
+  {
+    Tensor<elt_t> R = fold(A1, 1, conj(A2), 1);
+    tensor::index a, b;
+    R.get_dimensions(&a,&b,&a,&b);
+    tensor::index a2 = a*a, b2 = b*b;
+    R = reshape(permute(R, 1, 2), a2, b2);
+    if (l) *l = a;
+    if (r) *r = b;
+    return R;
+  }
+
+}
---
+++ src/mps/tools/propagate_mps.hpp
@@ -0,0 +1,45 @@
+// -*- mode: c++; fill-column: 80; c-basic-offset: 2; indent-tabs-mode: nil -*-
+/*
+    Copyright (c) 2012 Juan Jose Garcia Ripoll
+
+    Tensor is free software; you can redistribute it and/or modify it
+    under the terms of the GNU Library General Public License as published
+    by the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Library General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*/
+
+#include <mps/tools.h>
+
+namespace mps {
+
+  template<class Tensor>
+  const Tensor
+  propagate_right(const Tensor &v, const Tensor &A, const Tensor &op)
+  {
+    /* v(a,a') A(a',i,b) */
+    v = fold(v, -1, A, 0);
+
+    /* v(a,i,b) Op(i,j) -> v(a,j,b) */
+    if (!op.is_empty()) {
+	v = foldin(op, -1, v, 1);
+    }
+
+    /* v(a,i,b) A*(a,i,b') l(b') -> v(b,b') */
+
+    tensor::index a,i,b;
+    v.get_dimensions(&a,&i,&b);
+    v = foldc(reshape(A, a*i,b), 0, reshape(v, a*i,b), 0);
+
+    return v;
+  }
+
+}
