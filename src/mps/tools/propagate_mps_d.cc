404071965f13e7382b3e528119042afe2819615a
Juan Jose Garcia Ripoll <jjgarcia@users.sourceforge.net>
2012-09-30 23:10:56 +0200

    Add missing files to src/mps

---
+++ src/mps/tools/propagate_mps_d.cc
@@ -0,0 +1,35 @@
+// -*- mode: c++; fill-column: 80; c-basic-offset: 2; indent-tabs-mode: nil -*-
+/*
+    Copyright (c) 2012 Juan Jose Garcia Ripoll
+
+    Tensor is free software; you can redistribute it and/or modify it
+    under the terms of the GNU Library General Public License as published
+    by the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Library General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*/
+
+#include "propagate_mps.hpp"
+
+namespace mps {
+
+using namespace tensor;
+
+/**\cond IGNORED */
+
+const RTensor propagate_right(const RTensor &v, const RTensor &A, const RTensor &op)
+{
+  return do_propagate_right(v, A, op);
+}
+
+/**\endcond */
+
+}
