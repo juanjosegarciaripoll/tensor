3e6b5871079251227016b21de73874700d3a180b
Juan Jose Garcia Ripoll <jjgarcia@users.sourceforge.net>
2013-03-17 18:33:20 +0100

    Implemented split_interactions()

--- src/mps/hamiltonian/split_interaction.cc
+++ src/mps/hamiltonian/split_interaction.cc
@@ -17,21 +17,31 @@
     51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 */
 
+#include <cmath>
+#include <tensor/linalg.h>
 #include <mps/quantum.h>
 #include <mps/hamiltonian.h>
 
 namespace mps {
 
+  using namespace tensor;
+
   void
   split_interaction(const CTensor &H12, std::vector<CTensor> *v1, std::vector<CTensor> *v2)
   {
+    assert(H12.rank() == 2);
+
     CTensor O1, O2;
-    index N = O1.dimension(-1);
-    v1->resize(N);
-    v2->resize(N);
+    RTensor s = linalg::svd(H12, &O1, &O2, SVD_ECONOMIC);
+
+    index N = sqrt(H12.dimension(0));
+    index n_op = s.size();
+    v1->resize(n_op);
+    v2->resize(n_op);
     for (index i = 0; i < N; i++) {
-      v1->at(i) = squeeze(O1(range(), range(), range(i)));
-      v2->at(i) = squeeze(O2(range(), range(), range(i)));
+      double sqrts = sqrt(s[i]);
+      v1->at(i) = sqrts * tensor::reshape(O1(range(), range(i)), N, N);
+      v2->at(i) = conj(sqrts * tensor::reshape(O2(range(), range(i)), N, N));
     }
   }
 
