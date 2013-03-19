95134c24d15f6afc5cabcb32b510cb02fb8e9f5a
Juan Jose Garcia Ripoll <jjgarcia@users.sourceforge.net>
2013-03-19 23:17:06 +0100

    Test the actual inner content of number/destruction_operator

--- test/test_quantum_fock.cc
+++ test/test_quantum_fock.cc
@@ -17,6 +17,7 @@
     51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 */
 
+#include "loops.h"
 #include <tensor/io.h>
 #include <mps/quantum.h>
 #include <gtest/gtest.h>
@@ -26,12 +27,24 @@ using namespace mps;
 TEST(Fock, Number) {
   {
     RSparse n0 = number_operator(0);
+    EXPECT_TRUE(all_equal(n0.priv_row_start(),
+                          igen << 0 << 0));
+    EXPECT_EQ(n0.priv_column().size(), 0);
+    EXPECT_EQ(n0.priv_data().size(), 0);
+
     RTensor n0_manual(igen << 1 << 1,
                       rgen << 0.0);
     EXPECT_TRUE(all_equal(n0, n0_manual));
   }
   {
     RSparse n1 = number_operator(1);
+    EXPECT_TRUE(all_equal(n1.priv_row_start(),
+                          igen << 0 << 0 << 1));
+    EXPECT_TRUE(all_equal(n1.priv_column(),
+                          igen << 1));
+    EXPECT_TRUE(all_equal(n1.priv_data(),
+                          rgen << 1.0));
+
     RTensor n1_manual(igen << 2 << 2,
                       rgen << 0.0 << 0.0
                            << 0.0 << 1.0);
@@ -39,6 +52,13 @@ TEST(Fock, Number) {
   }
   {
     RSparse n2 = number_operator(2);
+    EXPECT_TRUE(all_equal(n2.priv_row_start(),
+                          igen << 0 << 0 << 1 << 2));
+    EXPECT_TRUE(all_equal(n2.priv_column(),
+                          igen << 1 << 2));
+    EXPECT_TRUE(all_equal(n2.priv_data(),
+                          rgen << 1.0 << 2.0));
+
     RTensor n2_manual(igen << 3 << 3,
                       rgen << 0.0 << 0.0 << 0.0
                            << 0.0 << 1.0 << 0.0
@@ -50,12 +70,24 @@ TEST(Fock, Number) {
 TEST(Fock, Creation) {
   {
     RSparse a0 = creation_operator(0);
+    EXPECT_TRUE(all_equal(a0.priv_row_start(),
+                          igen << 0 << 0));
+    EXPECT_EQ(a0.priv_column().size(), 0);
+    EXPECT_EQ(a0.priv_data().size(), 0);
+
     RTensor a0_manual(igen << 1 << 1,
                       rgen << 0.0);
     EXPECT_TRUE(all_equal(a0, a0_manual));
   }
   {
     RSparse a1 = creation_operator(1);
+    EXPECT_TRUE(all_equal(a1.priv_row_start(),
+                          igen << 0 << 0 << 1));
+    EXPECT_TRUE(all_equal(a1.priv_column(),
+                          igen << 0));
+    EXPECT_TRUE(all_equal(a1.priv_data(),
+                          rgen << 1.0));
+
     RTensor a1_manual(igen << 2 << 2,
                       rgen << 0.0 << 1.0
                            << 0.0 << 0.0);
@@ -63,6 +95,13 @@ TEST(Fock, Creation) {
   }
   {
     RSparse a2 = creation_operator(2);
+    EXPECT_TRUE(all_equal(a2.priv_row_start(),
+                          igen << 0 << 0 << 1 << 2));
+    EXPECT_TRUE(all_equal(a2.priv_column(),
+                          igen << 0 << 1));
+    EXPECT_TRUE(all_equal(a2.priv_data(),
+                          rgen << 1.0 << sqrt(2.0)));
+
     RTensor a2_manual(igen << 3 << 3,
                       rgen << 0.0 << 1.0 << 0.0
                            << 0.0 << 0.0 << sqrt(2.0)
