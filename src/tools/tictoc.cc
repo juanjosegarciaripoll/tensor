63789543f57b7d14e775082e7f9afcc816962a76
Juanjo Garcia-Ripoll <jjgarcia@users.sourcerforge.net>
2013-04-05 11:19:40 +0200

    tic/toc were not compiled in the tensor namespace

--- src/tools/tictoc.cc
+++ src/tools/tictoc.cc
@@ -20,23 +20,27 @@
 #include <ctime>
 #include <tensor/tools.h>
 
-static clock_t tic_start;
+namespace tensor {
 
-/**Reset the time counter.*/
-void tic()
-{
-  tic_start = clock();
-}
+  static clock_t tic_start;
 
-/**Output the time in seconds since last invocation of tic(). This function
-   only counts the processing time that the program has used since the last
-   call of tic(). If there are more than one program running in the computer,
-   or if your program makes use of functions such as sleep() to make pauses,
-   this time will be much shorter than the real elapsed time.
+  /**Reset the time counter.*/
+  void tic()
+  {
+    tic_start = clock();
+  }
 
-   Opposite to Matlab, toc() by itself does not produce any informative message.
-*/
-double toc()
-{
-  return (clock()-tic_start)/((double)CLOCKS_PER_SEC);
+  /**Output the time in seconds since last invocation of tic(). This function
+     only counts the processing time that the program has used since the last
+     call of tic(). If there are more than one program running in the computer,
+     or if your program makes use of functions such as sleep() to make pauses,
+     this time will be much shorter than the real elapsed time.
+
+     Opposite to Matlab, toc() by itself does not produce any informative message.
+  */
+  double toc()
+  {
+    return (clock()-tic_start)/((double)CLOCKS_PER_SEC);
+  }
+
 }
