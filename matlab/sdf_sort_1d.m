8dda1802301423f74698133d12ee7d14eff0ba26
Juanjo Garcia-Ripoll <jjgarcia@users.sourcerforge.net>
2013-06-28 10:12:04 +0200

    sdf_sort_1d() now accepts more than one field to sort.

--- matlab/sdf_sort_1d.m
+++ matlab/sdf_sort_1d.m
@@ -1,7 +1,11 @@
-function record = sdf_sort_1d(record, field)
+function record = sdf_sort_1d(record, varargin)
 names = fieldnames(record);
-data = getfield(record, field);
-[~, order] = sort(data);
+data = [];
+for i = 1:length(varargin)
+    aux = getfield(record, varargin{i});
+    data = [data, aux(:)];
+end
+[~, order] = sortrows(data);
 l = length(order);
 for i = 1:length(names)
     data = getfield(record, names{i});
@@ -10,11 +14,12 @@ for i = 1:length(names)
     k = find(dims > 1);
     k = k(end);
     if dims(k) ~= l
-        error(['The last dimension of field ' names{i} ' does not ' ...
-               'match the size of the sort field ' field]);
+        warn(['The last dimension of field ' names{i} ' does not ' ...
+              'match the size of the sort field ' field]);
+    else
+        data = reshape(data, numel(data)/l, l);
+        data = reshape(data(:,order), dims);
+        record = setfield(record, names{i}, data);
     end
-    data = reshape(data, numel(data)/l, l);
-    data = reshape(data(:,order), dims);
-    record = setfield(record, names{i}, data);
 end
 
