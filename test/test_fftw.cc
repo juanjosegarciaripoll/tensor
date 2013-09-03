cb02140a175af58d55ed0178a8d7c5f89bde2318
Juanjo Garcia-Ripoll <jjgarcia@users.sourcerforge.net>
2013-09-03 10:02:08 +0200

    Extended tests for FFTW

--- test/test_fftw.cc
+++ test/test_fftw.cc
@@ -25,21 +25,18 @@
 namespace tensor_test {
 
   // Creates the matrix to do the DFT in the slow way.
-  CTensor build_dft_transform(int N) {
-    RTensor vector(N, 1);
+  CTensor build_dft_transform(int N, int sign) {
+    RTensor vector = linspace(0, N, N);
+    vector = reshape(vector, N, 1);
 
-    for (int i = 0; i < N; i++) {
-      vector.at_seq(i) = i;
-    }
-
-    return exp(-(2*M_PI/N) * cdouble(0,1) * fold(vector, 1, vector, 1));
+    return exp(sign * (2*M_PI/N) * cdouble(0,1) * fold(vector, 1, vector, 1));
   }
 
   // DFT along all indices
-  CTensor full_dft(const CTensor& input) {
+  CTensor full_dft(const CTensor& input, int sign) {
     CTensor output(input);
     for (int dim = 0; dim < input.rank(); dim++) {
-      CTensor trafo = build_dft_transform(output.dimension(dim));
+      CTensor trafo = build_dft_transform(output.dimension(dim), sign);
       output = foldin(trafo, 1, output, dim);
     }
 
@@ -48,18 +45,65 @@ namespace tensor_test {
 
   TEST(FFTWTest, ForwardFFTTest) {
     for (int rank = 1; rank <= 3; rank++) {
-        for (DimensionIterator iter(rank,8); iter; ++iter) {
-          CTensor input = CTensor::random(*iter);
-          if (input.size() == 0) {
-            continue;
-          }
+      for (DimensionIterator iter(rank,8); iter; ++iter) {
+        CTensor input = CTensor::random(*iter);
 
-          CTensor fourier = fftw(input);
-          CTensor ref_fourier = full_dft(input);
-
-          EXPECT_TRUE(all_equal(input.dimensions(), fourier.dimensions()));
-          EXPECT_TRUE(approx_eq(ref_fourier, fourier, 1e-10));
+        if (input.size() == 0) {
+          continue;
         }
+
+        CTensor ref_fourier = full_dft(input, -1);
+
+        // test all variants of the forward FFT
+        CTensor fourier = fftw(input);
+        CTensor fft_forward = fftw(input, FFTW_FORWARD);
+        CTensor fourier_inplace = input;
+        CTensor fft_forward_inplace = input;
+        fftw_inplace(fourier_inplace);
+        fftw_inplace(fft_forward_inplace, FFTW_FORWARD);
+
+        EXPECT_TRUE(all_equal(input.dimensions(), fourier.dimensions()));
+        EXPECT_TRUE(all_equal(input.dimensions(), fft_forward.dimensions()));
+        EXPECT_TRUE(all_equal(input.dimensions(), fourier_inplace.dimensions()));
+        EXPECT_TRUE(all_equal(input.dimensions(), fft_forward_inplace.dimensions()));
+
+        EXPECT_TRUE(approx_eq(ref_fourier, fourier, 1e-10));
+        EXPECT_TRUE(approx_eq(ref_fourier, fft_forward, 1e-10));
+        EXPECT_TRUE(approx_eq(ref_fourier, fourier_inplace, 1e-10));
+        EXPECT_TRUE(approx_eq(ref_fourier, fft_forward_inplace, 1e-10));
+      }
+    }
+  }
+
+    TEST(FFTWTest, BackwardFFTTest) {
+    for (int rank = 1; rank <= 3; rank++) {
+      for (DimensionIterator iter(rank,8); iter; ++iter) {
+        CTensor input = CTensor::random(*iter);
+
+        if (input.size() == 0) {
+          continue;
+        }
+
+        CTensor ref_fourier = full_dft(input, +1);
+
+        // test all variants of the inverse FFT
+        CTensor fourier = ifftw(input);
+        CTensor fft_backward = fftw(input, FFTW_BACKWARD);
+        CTensor fourier_inplace = input;
+        CTensor fft_backward_inplace = input;
+        ifftw_inplace(fourier_inplace);
+        fftw_inplace(fft_backward_inplace, FFTW_BACKWARD);
+
+        EXPECT_TRUE(all_equal(input.dimensions(), fourier.dimensions()));
+        EXPECT_TRUE(all_equal(input.dimensions(), fft_backward.dimensions()));
+        EXPECT_TRUE(all_equal(input.dimensions(), fourier_inplace.dimensions()));
+        EXPECT_TRUE(all_equal(input.dimensions(), fft_backward_inplace.dimensions()));
+
+        EXPECT_TRUE(approx_eq(ref_fourier, fourier, 1e-10));
+        EXPECT_TRUE(approx_eq(ref_fourier, fft_backward, 1e-10));
+        EXPECT_TRUE(approx_eq(ref_fourier, fourier_inplace, 1e-10));
+        EXPECT_TRUE(approx_eq(ref_fourier, fft_backward_inplace, 1e-10));
+      }
     }
   }
 }
