2a1f5c9fa0e1d9b2c24be2e19e323d588957aefc
Juanjo Garcia-Ripoll <jjgarcia@users.sourceforge.net>
2014-06-19 16:26:00 +0200

    Fixed conditional inclusion of tensor/flags.h

--- include/tensor/flags.h
+++ include/tensor/flags.h
@@ -17,8 +17,8 @@
     51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 */
 
-#ifndef TENSOR_TOOLS_H
-#define TENSOR_TOOLS_H
+#ifndef TENSOR_FLAGS_H
+#define TENSOR_FLAGS_H
 
 #include <vector>
 
