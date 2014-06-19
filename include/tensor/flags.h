aabfc0a7253f03f97dca55cd4c641d31bfbf4b5a
Juanjo Garcia-Ripoll <jjgarcia@users.sourceforge.net>
2014-06-19 15:21:54 +0200

    Added flags with default values.

---
+++ include/tensor/flags.h
@@ -0,0 +1,49 @@
+// -*- mode: c++; fill-column: 80; c-basic-offset: 2; indent-tabs-mode: nil -*-
+/*
+    Copyright (c) 2010 Juan Jose Garcia Ripoll
+
+    Tensor is free software; you can redistribute it and/or modify it
+    under the terms of the GNU Library General Public License as published
+    by the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Library General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*/
+
+#ifndef TENSOR_TOOLS_H
+#define TENSOR_TOOLS_H
+
+#include <vector>
+
+namespace tensor {
+
+  class Flags {
+  public:
+    static const double DEFAULT;
+
+    Flags();
+
+    double get(unsigned int code) const;
+    double get(unsigned int code, double def) const;
+    Flags &set(unsigned int code, double value);
+
+    static unsigned int create_key();
+
+  private:
+    std::vector<double> _values;
+    static int last_key;
+  };
+
+  extern const Flags DEFAULT_FLAGS;
+  extern Flags TENSOR_FLAGS;
+
+} // namespace tensor
+
+#endif
---
+++ src/tools/flags.cc
@@ -0,0 +1,63 @@
+// -*- mode: c++; fill-column: 80; c-basic-offset: 2; indent-tabs-mode: nil -*-
+/*
+    Copyright (c) 2010 Juan Jose Garcia Ripoll
+
+    Tensor is free software; you can redistribute it and/or modify it
+    under the terms of the GNU Library General Public License as published
+    by the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Library General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*/
+
+#include <cmath>
+#include <tensor/flags.h>
+
+namespace tensor {
+
+  const double Flags::DEFAULT = NAN;
+  const Flags DEFAULT_FLAGS;
+  int Flags::last_key = -1;
+
+  Flags::Flags() :
+    _values(last_key+1, DEFAULT)
+  {
+  }
+
+  double Flags::get(unsigned int code) const
+  {
+    if (code >= _values.size())
+      return DEFAULT;
+    else
+      return _values[code];  
+  }
+
+  double Flags::get(unsigned int code, double def) const
+  {
+    double value = get(code);
+    if (value == DEFAULT)
+      return def;
+    return value;
+  }
+
+  class Flags &Flags::set(unsigned int code, double value)
+  {
+    if (_values.size() >= code)
+      _values.resize(code+1, DEFAULT);
+    _values.at(code) = value;
+    return *this;
+  }
+
+  unsigned int Flags::create_key()
+  {
+    return last_key++;
+  }
+
+}
