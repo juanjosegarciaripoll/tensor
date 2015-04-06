d2a04d0eb69939d996991fbe77cb804be0ae3f22
Juanjo Garcia-Ripoll <jjgarcia@users.sourceforge.net>
2015-04-06 15:21:25 +0200

    eig_power() would not converge when the eigenvalues have negative sign or some phase.

--- src/linalg/eig_power.hpp
+++ src/linalg/eig_power.hpp
@@ -43,22 +43,25 @@ namespace linalg {
     }
     v /= norm2(v);
     elt_t eig, old_eig;
+    //
+    // We apply repeatedly the map 'A' onto the same random initial
+    // vector, until (A^n)*v converges to the eigenstate with the largest
+    // eigenvalue (in absolute value) that has some support on 'v'.
+    // Sometimes degeneracy will slow convergence. In this case we stop
+    // when the algorithm slows downs too much.
+    //
     for (size_t i = 0; i <= iter; i++) {
       Tensor<elt_t> v_new = (*A)(v);
-      eig = scprod(v_new, v);
-      v_new /= norm2(v_new);
-      if (i) {
-        double eig_change = std::abs(eig - old_eig);
-        if (eig_change < tol * std::abs(eig)) {
-          double error = norm0(v_new - v);
-          if (error < tol) {
-            v = v_new;
-            break;
-          }
-        }
-      }
+      eig = scprod(v, v_new);
+      double err = norm0(v_new - eig * v);
+      v = (v_new /= norm2(v_new));
+      // Stop if the vector is sufficiently close to an eigenstate
+      if (err < tol * std::abs(eig))
+        break;
+      // Or if the eigenvalues stopped evolving
+      if (i && (std::abs(eig - old_eig) < tol * std::abs(eig)))
+        break;
       old_eig = eig;
-      v = v_new;
     }
     delete A;
     return eig;
