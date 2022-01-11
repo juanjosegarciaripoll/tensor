16478b3f53e2e8725829bd3f4528185313dd44b9
Juan Jose Garcia-Ripoll <juanjose.garciaripoll@gmail.com>
2022-01-11 18:04:05 +0100

    Add missing return() statement to tensor_openblas_requires_lapack

--- cmake/TensorOpenBLAS.cmake
+++ cmake/TensorOpenBLAS.cmake
@@ -6,18 +6,21 @@ function(tensor_openblas_requires_lapack)
   cmake_parse_arguments(ARGP "" "TARGET" "" ${ARGN})
   set(CMAKE_REQUIRED_LIBRARIES ${TARGET})
   check_c_source_compiles(
-    "int main() { extern void *dsgevd_; if (dsgevd != 0) return 0; return 1; }"
+    "int main() { extern void *dsgevd; if (dsgevd != 0) return 0; return 1; }"
     OPENBLAS_HAS_LAPACK
   )
   if (NOT OPENBLAS_HAS_LAPACK)
     check_c_source_compiles(
-        "int main() { extern void *dsgevd_; if (dsgevd != 0) return 0; return 1; }"
+        "int main() { extern void *dsgevd_; if (dsgevd_ != 0) return 0; return 1; }"
         OPENBLAS_HAS_LAPACK
     )
     if (NOT OPENBLAS_HAS_LAPACK)
+      message(STATUS "OpenBLAS requires separate LAPACK library")
       set(OPENBLAS_REQUIRES_LAPACK TRUE PARENT_SCOPE)
+      return()
     endif()
   endif()
+  message(STATUS "OpenBLAS does not require separate LAPACK library")
   set(OPENBLAS_REQUIRES_LAPACK FALSE PARENT_SCOPE)
 endfunction()
 
