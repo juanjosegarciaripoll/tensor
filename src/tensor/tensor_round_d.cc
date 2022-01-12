c4adf801f1e81d8680db3af912e1c444b2fbd3dd
Juan Jose Garcia-Ripoll <juanjose.garciaripoll@gmail.com>
2022-01-12 19:52:49 +0100

    Remove use of std::unary_function

--- src/tensor/tensor_round_d.cc
+++ src/tensor/tensor_round_d.cc
@@ -18,34 +18,21 @@
 
 #include <math.h>
 #include <cmath>
-#include <functional>
 #include <algorithm>
 #include <tensor/tensor.h>
 
 namespace tensor {
 
-#if defined(_MSC_VER) && (_MSC_VER < 1800)
-double tensor::round(double x) {
-  return floor((x < 0) ? (x - 0.5) : (x + 0.5));
-}
-#endif
-
-// Creation of a user-defined function object
-// that inherits from the unary_function base class
-class round1 : std::unary_function<double, double> {
- public:
-  result_type operator()(argument_type i) {
-#if defined(_MSC_VER) && (_MSC_VER < 1800)
-    return round(i);
-#else
-    return ::round(i);
-#endif
-  }
-};
-
 RTensor round(const RTensor &r) {
   RTensor output(r.dimensions());
-  std::transform(r.begin(), r.end(), output.begin(), round1());
+#if defined(_MSC_VER) && (_MSC_VER < 1800)
+  std::transform(r.begin(), r.end(), output.begin(), [](double x) {
+    return floor((x < 0) ? (x - 0.5) : (x + 0.5));
+  });
+#else
+  std::transform(r.begin(), r.end(), output.begin(),
+                 [](double x) { return ::round(x); });
+#endif
   return output;
 }
 
