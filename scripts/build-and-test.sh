842456865c43b3f44aeec35efb464e6a1df57633
Juan Jose Garcia-Ripoll <juanjose.garciaripoll@gmail.com>
2022-03-06 19:44:39 +0100

    Added more granularity to the build process

--- scripts/build-and-test.sh
+++ scripts/build-and-test.sh
@@ -1,7 +1,54 @@
-#!/bin/sh
-do_test=yes
-do_profile=yes
-do_delete=yes
+#!/bin/bash
+function clean () {
+    if [ $do_clean = yes ]; then
+        if [ -d "$builddir" ]; then
+            rm -rf "$builddir"
+        fi
+    fi
+}
+
+function configure () {
+    if [ $do_configure = yes ]; then
+        test -d "$builddir" || mkdir "$builddir"
+        CMAKE_FLAGS="-DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE} -DTENSOR_ARPACK=ON -DTENSOR_FFTW=ON -DTENSOR_OPTIMIZED_BUILD=ON"
+        cmake -H"$sourcedir" -B"$builddir" $CMAKE_FLAGS -G "$generator" 2>&1 | tee -a "$logfile"
+        if [ $? -ne 0 ]; then
+            echo CMake configuration failed
+            exit 1
+        fi
+    fi
+}
+
+function build () {
+    if [ $do_build = yes ]; then
+        cmake --build "$builddir" --config Release -j $threads -- 2>&1 | tee -a "$logfile"
+        if [ $? -ne 0 ]; then
+            echo CMake build failed
+            exit 1
+        fi
+    fi
+}
+
+function profile () {
+    if [ $do_profile = yes ]; then
+        "$builddir/profile/profile" "$sourcedir/profile/benchmark_$os.json" 2>&1 | tee -a "$logfile"
+        if [ $? -ne 0 ]; then
+            echo CMake profile failed
+            exit 1
+        fi
+    fi
+}
+
+function check () {
+    if [ $do_check = yes ]; then
+        cd "$builddir"/tests && ctest -j $threads | tee -a "$logfile"
+        if [ $? -ne 0 ]; then
+            echo CMake profile failed
+            exit -1
+        fi
+    fi
+}
+
 os=`uname -o`
 if [ -f /etc/os-release ]; then
    os=`(. /etc/os-release; echo $ID)`
@@ -13,60 +60,32 @@ logfile="$builddir/log"
 OPENBLAS_NUM_THREADS=4
 export OPENBLAS_NUM_THREADS
 CMAKE_BUILD_TYPE=Release
-for arg in $*; do
-    case $arg in
-        --no-test) do_test=no;;
-        --no-profile) do_profile=no;;
-        --continue) do_delete=no;;
-        --debug) CMAKE_BUILD_TYPE=Debug;;
-        --release) CMAKE_BUILD_TYPE=Release;;
-    esac
-done
 if test -n `which ninja`; then
     generator="Ninja"
 else
     generator="Unix Makefiles"
 fi
-if [ $do_delete = yes ]; then
-    if [ -d "$builddir" ]; then
-        rm -rf "$builddir"
-    fi
-fi
-#
-# Configure
-#
-test -d "$builddir" || mkdir "$builddir"
-CMAKE_FLAGS="-DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE} -DTENSOR_ARPACK=ON -DTENSOR_FFTW=ON -DTENSOR_OPTIMIZED_BUILD=ON"
-cmake -H"$sourcedir" -B"$builddir" $CMAKE_FLAGS -G "$generator" 2>&1 | tee -a "$logfile"
-if [ $? -ne 0 ]; then
-    echo CMake configuration failed
-    exit 1
-fi
-#
-# Build
-#
-cmake --build "$builddir" --config Release -j $threads -- 2>&1 | tee -a "$logfile"
-if [ $? -ne 0 ]; then
-    echo CMake build failed
-    exit 1
-fi
-#
-# Run profile
-#
-if [ $do_profile = yes ]; then
-    "$builddir/profile/profile" "$sourcedir/profile/benchmark_$os.json" 2>&1 | tee -a "$logfile"
-    if [ $? -ne 0 ]; then
-        echo CMake profile failed
-        exit 1
-    fi
-fi
-#
-# Run tests
-#
-if [ $do_test = yes ]; then
-    cd "$builddir"/tests && ctest -j $threads | tee -a "$logfile"
-    if [ $? -ne 0 ]; then
-        echo CMake profile failed
-        exit -1
-    fi
-fi
+
+do_clean=no
+do_configure=no
+do_build=no
+do_profile=no
+do_check=no
+for arg in $*; do
+    case $arg in
+        --clean) do_clean=yes;;
+        --configure) do_configure=yes;;
+        --build) do_build=yes;;
+        --profile) do_profile=yes;;
+        --test) do_check=yes;;
+        --all) do_clean=yes; do_configure=yes; do_build=yes; do_profile=yes; do_check=yes;;
+        --debug) CMAKE_BUILD_TYPE=Debug;;
+        --release) CMAKE_BUILD_TYPE=Release;;
+    esac
+done
+
+clean
+configure
+build
+check
+profile
