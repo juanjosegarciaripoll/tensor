491b6da39f3fd347318bf84d08a9fed46b9c526a
Juan Jose Garcia-Ripoll <juanjose.garciaripoll@gmail.com>
2022-03-06 12:34:47 +0100

    Operating system name deduction happened too late in build script

--- scripts/build-and-test.sh
+++ scripts/build-and-test.sh
@@ -3,15 +3,15 @@ do_test=yes
 do_profile=yes
 do_delete=yes
 os=`uname -o`
+if [ -f /etc/os-release ]; then
+   os=`(. /etc/os-release; echo $ID)`
+fi
 threads=10
 sourcedir=`pwd`
 builddir="$sourcedir/build-$os"
 logfile="$builddir/log"
 OPENBLAS_NUM_THREADS=4
 export OPENBLAS_NUM_THREADS
-if [ -f /etc/os-release ]; then
-   os=`(. /etc/os-release; echo $ID)`
-fi
 for arg in $*; do
     case $arg in
         --no-test) do_test=no;;
