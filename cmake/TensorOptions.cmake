610e9120cbad5ea7507342921fb83eec3466fbf9
Juan Jose Garcia-Ripoll <juanjose.garciaripoll@gmail.com>
2022-11-05 19:06:51 +0100

    Mark some CMake variables as advanced

--- cmake/TensorOptions.cmake
+++ cmake/TensorOptions.cmake
@@ -6,6 +6,13 @@ option(WARNINGS_AS_ERRORS "Compilation and analysis warnings become errors" OFF)
 option(TENSOR_USE_PCH "Use precompiled headers" ON)
 option(TENSOR_ADD_SANITIZERS "Compile and link with address sanitizers if in Debug mode" OFF)
 
+# The variables are not for ordinary users, so we hide them
+mark_as_advanced(TENSOR_DEFAULT_WARNINGS
+                 TENSOR_OPTIMIZED_BUILD
+                 TENSOR_CLANG_TIDY
+                 WARNINGS_AS_ERRORS
+                 TENSOR_ADD_SANITIZERS)
+
 function(make_tensor_options)
     add_library(tensor_options INTERFACE)
     if (TENSOR_OPTIMIZED_BUILD)
